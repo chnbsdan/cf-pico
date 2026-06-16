32ffcbda2789f426d13e658ce119fbbed2e9b494
chnbsdan <chnbsdan@gmail.com>
2026-06-16 21:00:30 +0800

    Create hello.js

---
+++ functions/api/hello.js
@@ -0,0 +1,8 @@
+export async function onRequest(context) {
+  return new Response('Hello from Cloudflare Pages Functions!', {
+    headers: {
+      'Content-Type': 'text/plain',
+      'Access-Control-Allow-Origin': '*'
+    }
+  })
+}
