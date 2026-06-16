7fb6722ada7b327bfcd53895749d341300333f2d
chnbsdan <chnbsdan@gmail.com>
2026-06-16 16:16:01 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // 确保是 '/'，不要用 './'
+  base: '/',  // 确保是 '/'
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false,
+  },
+  server: {
+    port: 5173,
+    host: true,
   },
 })
